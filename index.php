8baeab1122db092c62e55d8700cea14182c0191f
Anshul <81064196+Anshul9602@users.noreply.github.com>
2022-06-30 08:42:08 +0530

    phpfist web

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first PHP website</title>
+</head>
+<body>
+    <div class="container">
+        this is ansu first php web
+    <?php
+        echo"hello everyone";          
+    ?>
+    </div>
+</body>
+</html>
