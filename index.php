86f3add6e27c8e4715585629f60d9ec559264a94
Anshul <81064196+Anshul9602@users.noreply.github.com>
2022-06-30 18:17:41 +0530

    add data of assing op

--- index.php
+++ index.php
@@ -13,18 +13,25 @@
 
     <?php
         echo"hello everyone";          
-        $variabel1 =24;
-        $variabel2 =28;
-        echo $variabel1;
+        $variable1 =24;
+        $variable2 =28;
+        echo $variable1
         echo "<br>";
-        echo $variabel2;
+        echo $variable2;
         echo "<br>";
         // addition operator
-        echo"the value of varibel1 + variabel2 is:";
-        echo $variabel1+$variabel2;
+        echo"the value of varible1 + variable2 is:";
+        echo $variable1+$variable2;
         echo "<br>";
-        echo"the value of varibel1 * variabel2 is:";
-        echo $variabel1*$variabel2;
+        echo"the value of varible1 * variable2 is:";
+        echo $variable1 * $variable2;
+        // assingment op
+        $newVar = $variable1;
+        $newVar += 1;
+        echo "the value of new variable is:";
+        echo $newVar;
+        echo "<br>";
+        
     ?>
     </div>
 </body>
